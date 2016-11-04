7a7308d62c5d9e3a65fdf7509f4d288103e2b45f
smanx <zc@mac-mini.local>
2016-11-04 14:43:46 +0800

    test

--- Touch ID Demo/ViewController.m
+++ Touch ID Demo/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import <LocalAuthentication/LocalAuthentication.h>
 @interface ViewController ()
 
 @end
@@ -16,13 +16,105 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self authenticateUser];
+    
 }
-
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)authenticateUser
+{
+    //初始化上下文对象
+    LAContext* context = [[LAContext alloc] init];
+    //错误对象
+    NSError* error = nil;
+    NSString* result = @"Authentication is needed to access your notes.";
+    
+    //首先使用canEvaluatePolicy 判断设备支持状态
+    if ([context canEvaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics error:&error]) {
+        //支持指纹验证
+        [context evaluatePolicy:LAPolicyDeviceOwnerAuthenticationWithBiometrics localizedReason:result reply:^(BOOL success, NSError *error) {
+            if (success) {
+                //验证成功，主线程处理UI
+            }
+            else
+            {
+                NSLog(@"%@",error.localizedDescription);
+                switch (error.code) {
+                    case LAErrorSystemCancel:
+                    {
+                        NSLog(@"Authentication was cancelled by the system");
+                        //切换到其他APP，系统取消验证Touch ID
+                        break;
+                    }
+                    case LAErrorUserCancel:
+                    {
+                        NSLog(@"Authentication was cancelled by the user");
+                        //用户取消验证Touch ID
+                        break;
+                    }
+                    case LAErrorUserFallback:
+                    {
+                        NSLog(@"User selected to enter custom password");
+                        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+                            //用户选择其他验证方式，切换主线程处理
+                        }];
+                        break;
+                    }
+                    default:
+                    {
+                        [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+                            //其他情况，切换主线程处理
+                        }];
+                        break;
+                    }
+                }
+            }
+        }];
+    }
+    else
+    {
+        //不支持指纹识别，LOG出错误详情
+        
+        switch (error.code) {
+            case LAErrorTouchIDNotEnrolled:
+            {
+                NSLog(@"TouchID is not enrolled");
+                break;
+            }
+            case LAErrorPasscodeNotSet:
+            {
+                NSLog(@"A passcode has not been set");
+                break;
+            }
+            default:
+            {
+                NSLog(@"TouchID not available");
+                break;
+            }
+        }
+        
+        NSLog(@"%@",error.localizedDescription);
+    }
+    
+//    //授权失败
+//    LAErrorAuthenticationFailed = kLAErrorAuthenticationFailed,
+//    
+//    //用户取消Touch ID授权
+//    LAErrorUserCancel           = kLAErrorUserCancel,
+//    
+//    //用户选择输入密码
+//    LAErrorUserFallback         = kLAErrorUserFallback,
+//    
+//    //系统取消授权(例如其他APP切入)
+//    LAErrorSystemCancel         = kLAErrorSystemCancel,
+//    
+//    //系统未设置密码
+//    LAErrorPasscodeNotSet       = kLAErrorPasscodeNotSet,
+//    
+//    //设备Touch ID不可用，例如未打开
+//    
+//    LAErrorTouchIDNotAvailable  = kLAErrorTouchIDNotAvailable,
+//    
+//    //设备Touch ID不可用，用户未录入
+//    LAErrorTouchIDNotEnrolled   = kLAErrorTouchIDNotEnrolled,
 }
 
 
