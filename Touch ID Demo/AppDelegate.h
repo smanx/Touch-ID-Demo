74dde743f9336b8bc2187ab3e74adb720a4a9f2d
smanx <zc@mac-mini.local>
2016-11-04 14:38:46 +0800

    Initial Commit

---
+++ Touch ID Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Touch ID Demo
+//
+//  Created by ZC on 16/11/4.
+//  Copyright © 2016年 ZC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Touch ID Demo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Touch ID Demo
+//
+//  Created by ZC on 16/11/4.
+//  Copyright © 2016年 ZC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
